9e7b56193e22d0feb7b8878bfb57bd5b23eed844
Arwa Aloraibi <arwa7oraibi@gmail.com>
2025-10-17 12:25:25 +0300

    fixing the day place

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -5,8 +5,9 @@ import WeatherData from './WeatherData';
 const WeatherForecast = ({ day, img, imgAlt, conditions, time }) => {
   return (
     <div className="weather">
+      <h2>{day}</h2>
       <WeatherIcon img={img} imgAlt={imgAlt} />
-      <WeatherData day={day} conditions={conditions} time={time} />
+      <WeatherData conditions={conditions} time={time} />
 
     </div>
   );
